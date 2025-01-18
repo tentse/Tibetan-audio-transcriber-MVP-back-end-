6f5f2f62a7ebef93ba316eb7cb0a7ae5a01f97a7
Tenzin Tsering <tenzintsering630@gmail.com>
2025-01-18 09:00:52 +0530

    file downloading and processing from celery

--- src/celery_task/task.py
+++ src/celery_task/task.py
@@ -8,10 +8,14 @@ import asyncio
 
 
 @celery_app.task(bind=True)
-def speech_to_text_task(self, content: str, email: str, model: str, project_id: str, file_url: str):
+def speech_to_text_task(self, email: str, model: str, project_id: str, file_url: str):
     try:
 
-        
+        print('downloading and getting audio file content')
+        content = asyncio.run(download_file_from_s3(file_url))
+        print('got audio file content')
+
+        print(content)
 
         # Run async function using asyncio.run
         audio_time_stamp = asyncio.run(get_time_stamp(content))  # Running async code inside sync task
--- src/routes/project.py
+++ src/routes/project.py
@@ -27,7 +27,7 @@ async def get_projects(email: str, db: Session = Depends(get_db)):
     return projects
 
 
-@router.post('/create/{email}/{model}')
+@router.post('/create')
 async def create_project(email: str, file: UploadFile, model: str, db: Session = Depends(get_db)):
     contents = await file.read()
     await file.seek(0)
@@ -40,17 +40,13 @@ async def create_project(email: str, file: UploadFile, model: str, db: Session =
     project_id = str(uuid.uuid4())
 
     upload_time = milliseconds_since_epoch = int(time.time() * 1000)
-    print(upload_time)
+    # print(upload_time)
 
-    print(file.filename)
+    # print(file.filename)
     
     upload_file_url = await upload_file_to_s3(file, file.filename.split('.')[-1], f"{upload_time}-{file.filename}")
 
-    print('before downloading')
-    file_obj = await download_file_from_s3(upload_file_url)
-    print('after downloading')
-
-    # task_execution_to_celery = speech_to_text_task.delay(contents, email, model, project_id, upload_file_url)
+    task_execution_to_celery = speech_to_text_task.delay(email, model, project_id, upload_file_url)
 
     created_date = date.today()
     # create a new project
@@ -116,7 +112,7 @@ async def download_project(email: str, project_id: str, format: str, db: Session
 
         txt_stream = io.StringIO(txt_content)
 
-        return StreamingResponse(txt_stream, media_type="text/plain", headers={"Content-Disposition": f"attachment; filename={project_name}.txt"})
+        return StreamingResponse(txt_stream, media_type="text/plain", headers={"Content-Disposition": f"attachment; filename={project_name}_text.txt"})
     elif (format == 'srt'):
         srt_content = ""
         index = 1
@@ -126,22 +122,15 @@ async def download_project(email: str, project_id: str, format: str, db: Session
             Converts seconds to SRT time format (HH:MM:SS,mmm).
             """
             ms = int((seconds % 1) * 1000)  # Extract milliseconds from the decimal part
-            s = int(seconds) % 60
-            m = int(seconds // 60) % 60
-            h = int(seconds // 3600)
-            return f"{h:02d}:{m:02d}:{s:02d},{ms:03d}"
+            s = int(seconds) % 60           # get in seconds
+            m = int(seconds // 60) % 60     # get in minutes
+            h = int(seconds // 3600)        # get in hours
+            return f"{h:02d}:{m:02d}:{s:02d},{ms:03d}"  # Format the time
         
 
         for subtitle in audio_inference_dicts:
             start_val = float(subtitle['start_time'])  # parse float
             end_val = float(subtitle['end_time'])      # parse float
-
-            # start_secs = int(start_val)                # integer part
-            # end_secs = int(end_val)
-
-            # start_millis = int(round((start_val - start_secs) * 1000))
-
-            # end_millis = int(round((end_val - end_secs) * 1000))
 
             format_start_time = seconds_to_srt_format(start_val)
             format_end_time = seconds_to_srt_format(end_val)
@@ -157,7 +146,7 @@ async def download_project(email: str, project_id: str, format: str, db: Session
         return StreamingResponse(
             srt_stream,
             media_type="application/x-subrip-text-subtitle",  # MIME type for SRT files
-            headers={"Content-Disposition": f"attachment; filename={project_name}.srt"}
+            headers={"Content-Disposition": f"attachment; filename={project_name}_subtitle.srt"}
         )
     else:
         return {"message": "Invalid format"}
